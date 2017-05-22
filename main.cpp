7833fa0d079cefd3d52642c6d53d1b29c80dc92e
Mäkelä Miika <makelam6@force.org.aalto.fi>
2017-05-22 20:17:35 +0300

    started on dlib

--- main.cpp
+++ main.cpp
@@ -3,9 +3,12 @@
 #include <utility>
 #include <iostream>
 
+#include "dlib/dlib/matrix.h"
+
+using namespace dlib;
 
 int main() {
-	
+	/* 
 	Vec2<double> a = Vec2<double>(std::make_pair(2,1) );
 	
 	Vec2<double> b = Vec2<double>(2,2);
@@ -24,9 +27,36 @@ int main() {
 	
 	std::cout << "sum vector: " << c << std::endl;
 	
-	std::cout << "vector d: " << d << std::endl;
+	std::cout << "vector d: " << d << std::endl; */
 	
 	
+	//time for dlib matrix stuff
+	
+	dlib::matrix<double,3,1> y;
+	
+	dlib::matrix<double,3,3> M;
+	
+	M = 1, 2,3,
+		2, 3, 4,
+		4, 6, 7;
+		
+	y = 3,4,5;
+	
+	dlib::matrix<double> x = dlib::inv(M)*y;
+	
+	std::cout << "x: \n" << x << std::endl;		
+	
+	std::cout <<  "M: \n" << M << std::endl;
+	
+	dlib::matrix<double> A = dlib::diag(M);
+	
+	std::cout <<  "A: \n" << A << std::endl;	
+	
+	eigenvalue_decomposition<matrix<double>> B = eigenvalue_decomposition<matrix<double>>(M);
+	
+	//complex_matrix<std::complex<double>>  = complex_matrix<std::complex<double>>(M); // = B.get_eigenvalues();
+	
+	//std::cout <<  "C: \n" << A << std::endl;
 	
 	return 0;
 	
